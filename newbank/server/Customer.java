2c410173c0bd8dbaeb7f46c3645be49af8da9562
SamTakhar12 <74096417+SamTakhar12@users.noreply.github.com>
2020-11-28 13:46:07 +0000

    Added ID and Password in Customer Constructor

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -3,18 +3,35 @@ package newbank.server;
 import java.util.ArrayList;
 
 public class Customer {
-	
+
 	private ArrayList<Account> accounts;
-	
-	public Customer() {
+	private Integer ID;
+	private String CustomerPassword;
+
+	public Customer(Integer ID) {
 		accounts = new ArrayList<>();
+		this.ID=ID;
 	}
-	
+	public Integer ID(){
+		return ID;
+	}
+
+
+	public String accountsToString() {
+		String s = "";
+		if (accounts.size() == 0) {
+			return "No accounts found for this user";
+		}
+		for(Account a : accounts) {
+			s += a.toString();
+		}
+		return s;
+	}
+
 	public void printAccountBalance() {
 		MenuPrinter.printShowAccounts(accounts);
 	}
-
 	public void addAccount(Account account) {
-		accounts.add(account);		
+		accounts.add(account);
 	}
 }
