ad3784160cc4ea012da47fb3c2a66916c8ebabd0
SamTakhar12 <74096417+SamTakhar12@users.noreply.github.com>
2020-11-28 13:47:58 +0000

    Amended Password method in Newbank. Also added Hashmap for linking Customer and password.

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -1,51 +1,65 @@
 package newbank.server;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class NewBank {
-	
+
 	private static final NewBank bank = new NewBank();
+	private HashMap<String, Integer> ID;
 	private static HashMap<String,Customer> customers;
-	
+	private HashMap<String,CustomerPassword> LoginCred;
 	private NewBank() {
+		ID = new HashMap<>();
 		customers = new HashMap<>();
+		LoginCred = new HashMap<>();
+
 		addTestData();
 	}
-	
-	private void addTestData() {
-		Customer bhagy = new Customer();
-		bhagy.addAccount(new Account("Main", 1000.0));
-		bhagy.addAccount(new Account("Savings", 20.0));
-		customers.put("Bhagy", bhagy);
 
-		// Tests a customer with no accounts
-		Customer christina = new Customer();
+	private void addTestData() {
+		Customer bhagy = new Customer(01);
+		bhagy.addAccount(new Account("Main", 1000.0));
+		customers.put("Bhagy", bhagy);
+		LoginCred.put("Bhagy",new CustomerPassword("Bhagy123"));
+
+		Customer christina = new Customer(02);
+		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
-		Customer john = new Customer();
+		LoginCred.put("Christina",new CustomerPassword("Christina123"));
+
+		Customer john = new Customer(03);
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
+		LoginCred.put("John",new CustomerPassword("John123"));
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
+
+	//Method which handles authentication
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
 		if(customers.containsKey(userName)) {
-			return new CustomerID(userName);
+			if(LoginCred.get(userName).verifyPassword(password))
+				return new CustomerID(userName);
 		}
 		return null;
 	}
 
-	public synchronized String validator(CustomerID customer) {
+
+
+
+	/*public synchronized String validator(CustomerID customer) {
 		if(customers.containsKey(customer.getKey())) {
 			return "valid";
 		}
 		return "invalid";
-	}
+	}*/
 
 	public static void showMyAccounts(CustomerID customer) {
 		customers.get(customer.getKey()).printAccountBalance();
 	}
 }
+
