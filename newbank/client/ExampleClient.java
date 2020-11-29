c30da99e820ceed38e756e9ab33016d5d9b2b244
SamTakhar12 <74096417+SamTakhar12@users.noreply.github.com>
2020-11-29 20:18:49 +0000

    Option to log out from Example Client

--- newbank/client/ExampleClient.java
+++ newbank/client/ExampleClient.java
@@ -43,6 +43,11 @@ public class ExampleClient extends Thread{
 				while(true) {
 					String command = userInput.readLine();
 					bankServerOut.println(command);
+					if(command.equals("LOGOUT")){
+					bankServerOut.close();
+					server.close();
+					System.exit(0);
+					}
 				}				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
