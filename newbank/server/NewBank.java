9543cc27cbab847b5236c3235fcf7386fb358c29
SamTakhar12 <74096417+SamTakhar12@users.noreply.github.com>
2020-11-29 20:22:04 +0000

    Modified Customer Password method for login

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -6,34 +6,42 @@ public class NewBank {
 	
 	private static final NewBank bank = new NewBank();
 	private static HashMap<String,Customer> customers;
+	private HashMap<String, Integer> ID;
+	private HashMap<String,CustomerPassword> LoginCred;
 	
 	private NewBank() {
+		ID = new HashMap<>();
 		customers = new HashMap<>();
+		LoginCred = new HashMap<>();
 		addTestData();
 	}
-	
-	private void addTestData() {
-		Customer bhagy = new Customer();
-		bhagy.addAccount(new Account("Main", 1000.0));
-		bhagy.addAccount(new Account("Savings", 20.0));
-		customers.put("Bhagy", bhagy);
 
-		// Tests a customer with no accounts
-		Customer christina = new Customer();
+	private void addTestData() {
+		Customer bhagy = new Customer(01);
+		bhagy.addAccount(new Account("Main", 1000.0));
+		customers.put("Bhagy", bhagy);
+		LoginCred.put("Bhagy",new CustomerPassword("Bhagy123"));
+
+		Customer christina = new Customer(02);
+		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
-		Customer john = new Customer();
+		LoginCred.put("Christina",new CustomerPassword("Christina123"));
+
+		Customer john = new Customer(03);
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
+		LoginCred.put("John",new CustomerPassword("John123"));
 	}
 	
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
+	//Method which handles authentication
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
 		if(customers.containsKey(userName)) {
-			return new CustomerID(userName);
+			if(LoginCred.get(userName).verifyPassword(password))
+				return new CustomerID(userName);
 		}
 		return null;
 	}
