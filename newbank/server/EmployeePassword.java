6cdaa6fe825cab4a25df7366b1d94a8289e8e694
Miles Eu <mileseu@me.com>
2020-12-05 17:18:06 +0000

    Employee class
    Employee class and login
    Login shows options for creating accounts and users.

---
+++ newbank/server/EmployeePassword.java
@@ -0,0 +1,14 @@
+package newbank.server;
+
+
+public class EmployeePassword {
+    private final String password;
+    public EmployeePassword(String password) {
+        this.password = password;
+    }
+
+    public boolean verifyPassword(String password){
+        return this.password.equals(password);
+    }
+
+}
