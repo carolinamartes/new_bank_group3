17868e890b112788cd857e23c0eeab66dcc19fbf
SamTakhar12 <74096417+SamTakhar12@users.noreply.github.com>
2020-12-06 14:01:10 +0000

    Added the methods for changing password and printing account balances to example client.

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -1,5 +1,7 @@
 package newbank.server;
 
+import newbank.client.ExampleClient;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -44,14 +46,14 @@ public class NewBankClientHandler extends Thread {
 				return customer;
 			out.println("Invalid Username or Password");
 			attempt++;
-		}
-		System.out.println("You have entered an incorrect User or password three times, please try again later.");
-		System.exit(-1);
-		return null;
 
+		}
+		out.println("Maximum Number of login attempts for this season reached");
+		return null;
 	}
 
-	public void run() {
+
+	public void run()  {
 
 		menuPrinter.printLogo();
 
@@ -59,37 +61,31 @@ public class NewBankClientHandler extends Thread {
 		try {
 
 			// ask for user name and password
-			out.println("Enter Username");
-			String userName = in.readLine();
-			out.println("Enter Password");
-			String password = in.readLine();
-			out.println("Checking Details...");
-
-			// authenticate user and get customer ID token from bank for use in subsequent requests
-			CustomerID customer = bank.checkLogInDetails(userName, password);
-
-			// if the user is authenticated then get requests from the user and process them
-			if(customer != null) {
-
+			//Salutation
+			out.println("Welcome to NewBank");
+			String entry = "";
+			CustomerID customer = null;
+			customer = login();
+			if (customer != null) {
 				out.println("Log In Successful. What do you want to do?");
 
-				while(true) {
+
+				while (true) {
 
 					String request = in.readLine();
 
 					System.out.println("Request from " + customer.getKey());
 					processRequest(customer, request);
 
-					}
+				}
 
-			}
-			else {
+			} else {
 				out.println("Log In Failed");
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				in.close();
 				out.close();
@@ -100,8 +96,8 @@ public class NewBankClientHandler extends Thread {
 		}
 	}
 
-	public synchronized void processRequest(CustomerID customer, String request) {
-		if (request.equals("1") || request.equals("2")){
+	public synchronized void processRequest(CustomerID customer, String request) throws IOException {
+		if (request.equals("1") || request.equals("2")) {
 			System.out.println("HERE");
 			if (state.peek().equals("TRANSFERFROM")) {
 				fromAccountIndex = Integer.parseInt(request) - 1;
@@ -110,56 +106,89 @@ public class NewBankClientHandler extends Thread {
 				toAccountIndex = Integer.parseInt(request) - 1;
 			}
 		}
-		if (request.startsWith("TRANSFERAMOUNT")){
+		if (request.startsWith("TRANSFERAMOUNT")) {
 			String requestString = request;
 			requestAmount = Double.parseDouble(requestString.replace("TRANSFERAMOUNT ", ""));
 			NewBank.showTransferFromOptions(customer, requestAmount);
 			state.push("TRANSFERFROM");
 			return;
 		}
+		if (request.startsWith("CHANGEPASSWORD")) {
+			out.println("Enter your userName");
+			String userName = in.readLine();
+			out.println("Enter your current password");
+			String Currentpassword = in.readLine();
+			CustomerID PasswordChange = bank.checkLogInDetails(userName, Currentpassword);
+			if (customer == null) {
+				out.println("user not recognized");
+			}
+			else{
+				out.println("Enter your new password, this must be a minimum of 8 characters");
+				String temp = in.readLine();
+				if (temp.length() <= 7 || Currentpassword.equals(temp)) {
+					out.println("Something went wrong");
+					return;
+				} else {
+					out.println("Please re-enter your new password");
+					String temp2 = in.readLine();
+					if (temp.equals(temp2)) {
+						CustomerID ID = bank.ChangePassword(userName, temp, Currentpassword);
+						out.println("Password Successfully Changed");
+						return;
+					}
+				}
+			}
+		}
+
 		switch (request) {
-			case "SHOWMYACCOUNTS" :
+			case "SHOWMYACCOUNTS":
 				NewBank.showMyAccounts(customer);
 				state.push(request);
 				break;
-			case "MOVEMYMONEY" :
+			case "PRINTACCOUNTS":
+				NewBank.printaccountstotext(customer);
+				state.push(request);
+				break;
+			case "MOVEMYMONEY":
 				menuPrinter.askTransferQuantity();
 				state.push(request);
 				break;
-			case "NEWACCOUNT" :
+			case "NEWACCOUNT":
 				menuPrinter.printNewAccountsPg1();
 				state.push(request);
 				break;
-			case "LOGOUT" :
+			case "LOGOUT":
 				menuPrinter.printLogOut();
 				state.push(request);
 				break;
-			case "BACK" :
+			case "BACK":
 				request = state.pop();
 				processRequest(customer, request);
 				break;
-			case "1" :
-			case "2" :
-				if (state.peek().equals("NEWACCOUNT")){
+			case "1":
+			case "2":
+				if (state.peek().equals("NEWACCOUNT")) {
 					menuPrinter.printNewAccountsPg2();
 					break;
 				}
-				if (state.peek().equals("TRANSFERFROM")){
+				if (state.peek().equals("TRANSFERFROM")) {
 					NewBank.showTransferToOptions(customer, fromAccountIndex);
 					state.push("TRANSFERTO");
 					break;
 				}
-				if (state.peek().equals("TRANSFERTO")){
+				if (state.peek().equals("TRANSFERTO")) {
 					NewBank.executeTransfer(customer, fromAccountIndex, toAccountIndex, requestAmount);
 					break;
-				}
-				else {
+				} else {
 					out.println("Invalid Request");
 					break;
 				}
-			default :
+			default:
 				menuPrinter.printFail();
 				break;
 		}
 	}
 }
+
+
+
